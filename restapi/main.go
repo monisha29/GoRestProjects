dcfb00738024ebf782e7d5b29f5ccf605d601071
monisha29 <monishanair29@gmail.com>
2020-04-16 20:55:29 +0530

    first go rest

---
+++ restapi/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+//Book Struct
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+//Init books var as a slice Book struct
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+func main() {
+	//Init router
+	r := mux.NewRouter()
+
+	//Mock data
+	books = append(books, Book{ID: "1", Isbn: "23343", Title: "Book One", Author: &Author{Firstname: "Monisha", Lastname: "Nair"}})
+	books = append(books, Book{ID: "2", Isbn: "23343", Title: "Book Two", Author: &Author{Firstname: "Poonam", Lastname: "Mandraha"}})
+	books = append(books, Book{ID: "3", Isbn: "23343", Title: "Book Three", Author: &Author{Firstname: "Ashish", Lastname: "Sirothia"}})
+	books = append(books, Book{ID: "4", Isbn: "23343", Title: "Book Four", Author: &Author{Firstname: "Sonali", Lastname: "Sharma"}})
+	//Endpointss
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
